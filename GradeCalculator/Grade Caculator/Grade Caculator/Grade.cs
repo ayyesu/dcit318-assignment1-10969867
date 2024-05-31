bfd09f6d1968505e5386a9bbd9e859e776712bea
ayyesu <iamdanielkissiedu@gmail.com>
2024-05-31 21:29:47 +0000

    completed - Grade Calculator Console Application Completed

---
+++ GradeCalculator/Grade Caculator/Grade Caculator/Grade.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Caculator
+{
+    public class Grade
+    {
+        public int NumGrade { get; set; }
+
+        // Constructor
+        public Grade(int grade)
+        {
+            NumGrade = grade;
+        }
+
+        // Method to determine Grade Letter
+        public string GetGradeLetter()
+        {
+            if (NumGrade >= 90)
+            {
+               return "A";
+            }else if (NumGrade >= 80)
+            {
+                return "B";
+            }else if (NumGrade >= 70)
+            {
+                return "C";
+            }else if (NumGrade >= 60)
+            {
+                return "D";
+            }else
+            {
+                return "F";
+            }
+        }
+    }
+}
---
+++ GradeCalculator/Grade Caculator/Grade Caculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade_Caculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a Numerical Grade between 0 - 100: ");
+
+            // Get input from user
+            string input = Console.ReadLine();
+
+            int numGrade;
+
+            // Validating the input
+            if (int.TryParse(input, out numGrade) && numGrade >= 0 && numGrade <= 100)
+            {
+                // Creating a grade
+                Grade grade = new Grade(numGrade);
+
+                // Getting and dispaying grade letter
+                string gradeLetter = grade.GetGradeLetter();
+
+                Console.WriteLine($"Your Calculated Grade is: {gradeLetter}");
+
+                // Prevents Console from closing
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, Please enter a numerical grade between 0 and 100.");
+
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
