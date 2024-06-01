790676ef7dd5864afa91ae7bcfd7ee2de59a8b94
ayyesu <iamdanielkissiedu@gmail.com>
2024-06-01 12:17:27 +0000

    completed - Ticket Price Calculator project completed

---
+++ GradeCalculator/Ticket Price Calculator/Ticket Price Calculator/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Price_Calculator
+{
+     class Customer
+    {
+        public int Age { get; set; }
+
+        public Customer(int age)
+        {
+            Age = age;
+        }
+
+        public decimal GetTicketPrice()
+        {
+            if (Age >= 65 || Age <= 12)
+            {
+                return 7.00m;
+            }
+            else
+            {
+                return 10.00m;
+            }
+        }
+    }
+}
---
+++ GradeCalculator/Ticket Price Calculator/Ticket Price Calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticket_Price_Calculator
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Customer, How old are you? ");
+
+            string input = Console.ReadLine();
+
+            int numAge;
+
+            if (int.TryParse(input, out numAge) && numAge > 0)
+            {
+                Customer customer = new Customer(numAge);
+
+                decimal ticketPrice = customer.GetTicketPrice();
+
+                Console.WriteLine($"Your Price for the ticket is GHC{ticketPrice}");
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, Please provide the right age to process your ticket price");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
