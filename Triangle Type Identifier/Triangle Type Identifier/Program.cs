85704f90e12dfcc113d12cf37528d005b4d83132
ayyesu <iamdanielkissiedu@gmail.com>
2024-06-01 13:36:25 +0000

    completed - Triangle Type Identifier project completed

---
+++ Triangle Type Identifier/Triangle Type Identifier/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Type_Identifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Enter the length for the 3 (three) sides...");
+
+                Console.WriteLine("Side1:");
+
+                double side1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Side2:");
+
+                double side2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Side3");
+
+                double side3 = Convert.ToDouble(Console.ReadLine());
+
+
+                Triangle triangle = new Triangle(side1, side2, side3);
+
+                if (triangle.IsValidTriangle())
+                {
+                    string triangleType = triangle.DetermineTriangleType();
+
+                    if (triangleType == "Equilateral")
+                    {
+                        Console.WriteLine($"Triangle is an {triangleType} triangle because all sides are equal");
+                    }else if (triangleType == "Isosceles")
+                    {
+                        Console.WriteLine($"Triangle is a {triangleType} triangle because only two sides are equal");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Triangle is a {triangleType} triangle because none of the sides are equal");
+                    }
+                    
+
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("The sides doesn't form a valid triangle");
+
+                    Console.ReadLine();
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid inputs, The input for the sides doesn't fit to be a triangle");
+            }
+        }
+    }
+}
---
+++ Triangle Type Identifier/Triangle Type Identifier/Triangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle_Type_Identifier
+{
+    class Triangle
+    {
+        public double Side1 {  get; set; }
+
+        public double Side2 { get; set; }
+
+        public double Side3 { get; set; }
+
+
+        public Triangle(double side1, double side2, double side3)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+
+        public string DetermineTriangleType()
+        {
+            if (Side1 == Side2 && Side2 == Side3)
+            {
+                return "Equilateral";
+            }else if (Side1 == Side2 || Side1 == Side3 || Side2 == Side3)
+            {
+                return "Isosceles";
+            }
+            else
+            {
+                return "Scalene";
+            }
+        }
+
+        public bool IsValidTriangle()
+        {
+            return (Side1 > 0 && Side2 > 0 && Side3 > 0) &&
+                   (Side1 + Side2 > Side3) &&
+                   (Side1 + Side3 > Side2) &&
+                   (Side2 + Side3 > Side1);
+        }
+    }
+}
